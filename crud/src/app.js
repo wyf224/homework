e0b5194b6a4fc9c9658339d4e91d2aa71464411c
wyf224 <172643957@qq.com>
2020-03-21 12:17:29 +0800

    POST tasks

--- crud/src/app.js
+++ crud/src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const fs = require('fs')
 const app = express()
 const port = 3000
-
+app.use(express.json())
 
 app.get('/tasks', (req, res) => fs.readFile('./Todo.json', 'utf-8', (err, data) => {
     if(err){
@@ -13,6 +13,46 @@ app.get('/tasks', (req, res) => fs.readFile('./Todo.json', 'utf-8', (err, data) 
 }))
 
 
+const asyncReadFile = function(path){
+    return new Promise(
+        function(resolve, reject){
+            fs.readFile(path, 'utf-8', function(err, data){
+                if(err){
+                    reject(err)
+                }
+                resolve(data)
+            })
+         }).catch(err => {
+             return err
+    })
+}
+
+const asyncWriteFile = function(string, path){
+    return new Promise(function (resolve, reject){
+        fs.writeFile(path, string, function(err){
+            reject(err) 
+        })
+    }).catch(err => {
+        return err
+    })
+}
+
+const createTask = async (req, res) => {
+    const newTask = req.body
+    const file = await asyncReadFile('./Todo.json') 
+    const tasks = JSON.parse(file)
+    if(tasks.filter(v => v.id == newTask.id).length != 0){
+        res.status(400).send()
+    } else {
+        tasks.push(newTask)
+        await asyncWriteFile(JSON.stringify(tasks), './Todo.json')
+        res.status(201).send(tasks)
+    }
+}
+
+app.post('/tasks',createTask)
 
 app.listen(port, () => console.log(`Our server has been setup,  and listen on the 
 port: ${port}!`))
+
+exports.app = app
